c9863629db9ebb741c63b494da367b498ebff388
Vishal Agrawal <106560207+looper15@users.noreply.github.com>
2023-09-13 19:46:27 +0530

    Update gsapscroll-animation.js

--- gsapscroll-animation.js
+++ gsapscroll-animation.js
@@ -1,32 +1,34 @@
-// Function to load external JavaScript file
-function loadScript(src, callback) {
-  const script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = src;
-  script.onload = callback; // callback function to run after script loads
-  document.head.appendChild(script);
-}
+// Simple encryption and decryption using XOR bitwise operation
+const xorEncrypt = (str, key) => {
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    result += String.fromCharCode(str.charCodeAt(i) ^ key);
+  }
+  return result;
+};
 
-// Load sites.js and then execute the rest of the code
-loadScript('./sites.js', () => {
-  // Editable variable
-  const MYAPP_WORK_ON_BROWSER = undefined;
+// Encrypted URLs
+const MYAPP_SHOULD_WORK = {
+  [xorEncrypt('https://springpoint-dev-6e43510d71e330dd345ca57.webflow.io/', 42)]: true,
+};
 
-  // Minified script
-  (function() {
-    const n = /^((?!chrome|android).)*safari/i.test(navigator.userAgent),
-          o = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
-    const t = () => MYAPP_WORK_ON_BROWSER === undefined ? true : MYAPP_WORK_ON_BROWSER ? n : o;
-    document.addEventListener("DOMContentLoaded", () => {
-      const e = xorEncrypt(window.location.hostname, 42);
-      if (!MYAPP_SHOULD_WORK[e] || !t()) return;
-      const r = document.querySelector("[data-myapp-animated-element]");
-      if (!r) return;
-      window.addEventListener("scroll", () => {
-        const e = r.style.opacity === "1" || r.style.opacity === "";
-        r.style.opacity = e ? "0.1" : "1";
-        r.style.transform = e ? "translateY(-2px)" : "translateY(0px)";
-      });
+// Editable variables
+const MYAPP_WORK_ON_BROWSER = undefined;
+
+// Minified script
+(function(){
+  const n = /^((?!chrome|android).)*safari/i.test(navigator.userAgent),
+        o = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+  const t = () => MYAPP_WORK_ON_BROWSER === undefined ? true : MYAPP_WORK_ON_BROWSER ? n : o;
+  document.addEventListener("DOMContentLoaded", () => {
+    const e = xorEncrypt(window.location.hostname, 42);
+    if (!MYAPP_SHOULD_WORK[e] || !t()) return;
+    const r = document.querySelector("[data-myapp-animated-element]");
+    if (!r) return;
+    window.addEventListener("scroll", () => {
+      const e = r.style.opacity === "1" || r.style.opacity === "";
+      r.style.opacity = e ? "0.1" : "1";
+      r.style.transform = e ? "translateY(-2px)" : "translateY(0px)";
     });
-  })();
-});
+  });
+})();
