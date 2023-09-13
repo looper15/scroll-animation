9099f5fc91001a79d82ee9786c8199c76afb6d07
Vishal Agrawal <106560207+looper15@users.noreply.github.com>
2023-09-13 20:01:26 +0530

    Update animation.js

--- animation.js
+++ animation.js
@@ -1,34 +1,30 @@
-// Simple encryption and decryption using XOR bitwise operation
-const xorEncrypt = (str, key) => {
-  let result = '';
-  for (let i = 0; i < str.length; i++) {
-    result += String.fromCharCode(str.charCodeAt(i) ^ key);
-  }
-  return result;
-};
-
-// Encrypted URLs
-const MYAPP_SHOULD_WORK = {
-  [xorEncrypt('https://springpoint-dev-6e43510d71e330dd345ca57.webflow.io/', 42)]: true,
-};
-
 // Editable variables
+const MYAPP_SHOULD_WORK = {
+  "example.com": true,
+  "www.example.com": true,
+  "anotherexample.com": false,
+  "www.anotherexample.com": true,
+};
 const MYAPP_WORK_ON_BROWSER = undefined;
 
 // Minified script
-(function(){
+(function () {
   const n = /^((?!chrome|android).)*safari/i.test(navigator.userAgent),
-        o = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
-  const t = () => MYAPP_WORK_ON_BROWSER === undefined ? true : MYAPP_WORK_ON_BROWSER ? n : o;
+    o =
+      /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+  const t = () => {
+    if (MYAPP_WORK_ON_BROWSER === undefined) return !0;
+    return MYAPP_WORK_ON_BROWSER ? n : o;
+  };
   document.addEventListener("DOMContentLoaded", () => {
-    const e = xorEncrypt(window.location.hostname, 42);
+    const e = window.location.hostname;
     if (!MYAPP_SHOULD_WORK[e] || !t()) return;
     const r = document.querySelector("[data-myapp-animated-element]");
     if (!r) return;
     window.addEventListener("scroll", () => {
-      const e = r.style.opacity === "1" || r.style.opacity === "";
-      r.style.opacity = e ? "0.1" : "1";
-      r.style.transform = e ? "translateY(-2px)" : "translateY(0px)";
+      const e = "1" === r.style.opacity || "" === r.style.opacity;
+      (r.style.opacity = e ? "0.1" : "1"),
+        (r.style.transform = e ? "translateY(-2px)" : "translateY(0px)");
     });
   });
 })();
