7305bd29026218ae3e2fa88f807fc18f22413334
Vishal Agrawal <106560207+looper15@users.noreply.github.com>
2023-09-13 20:26:15 +0530

    Update animate.js

--- animate.js
+++ animate.js
@@ -1,28 +1,28 @@
-// Editable variables
-const MYAPP_SHOULD_WORK = {
-  "https://springpoint-dev-6e43510d71e330dd345ca57.webflow.io/": true,
-  "www.example.com": true,
-};
-const MYAPP_WORK_ON_BROWSER = undefined;
-
-// Minified script
 (function () {
-  const n = /^((?!chrome|android).)*safari/i.test(navigator.userAgent),
-    o =
-      /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
-  const t = () => {
-    if (MYAPP_WORK_ON_BROWSER === undefined) return !0;
-    return MYAPP_WORK_ON_BROWSER ? n : o;
+  // Replace the global SHOULD_WORK flag with an object
+  const MYAPP_SHOULD_WORK = {
+    "springpoint-dev-6e43510d71e330dd345ca57.webflow.io": true,
   };
+
+  const MYAPP_WORK_ON_BROWSER = undefined;
+
+  const MYAPP_IS_SAFARI = /^((?!chrome|android).)*safari/i.test(
+      navigator.userAgent
+    ),
+    MYAPP_IS_CHROME =
+      /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor),
+    isMyAppCompatibleBrowser = () =>
+      void 0 === MYAPP_WORK_ON_BROWSER ||
+      (MYAPP_WORK_ON_BROWSER ? MYAPP_IS_SAFARI : MYAPP_IS_CHROME);
   document.addEventListener("DOMContentLoaded", () => {
-    const e = window.location.hostname;
-    if (!MYAPP_SHOULD_WORK[e] || !t()) return;
-    const r = document.querySelector("[data-myapp-animated-element]");
-    if (!r) return;
-    window.addEventListener("scroll", () => {
-      const e = "1" === r.style.opacity || "" === r.style.opacity;
-      (r.style.opacity = e ? "0.1" : "1"),
-        (r.style.transform = e ? "translateY(-2px)" : "translateY(0px)");
-    });
+    let e = window.location.hostname;
+    if (!MYAPP_SHOULD_WORK[e] || !isMyAppCompatibleBrowser()) return;
+    let t = document.querySelector("[data-myapp-animated-element]");
+    t &&
+      window.addEventListener("scroll", () => {
+        let e = "1" === t.style.opacity || "" === t.style.opacity;
+        (t.style.opacity = e ? "0.1" : "1"),
+          (t.style.transform = e ? "translateY(-10px)" : "translateY(10px)");
+      });
   });
 })();
